be1bfa4cca2070f5fb8605afa66e958dc98bf990
time2developer <time2developer@gmail.com>
2024-04-10 11:49:35 +0530

    chatSection[BarChat responsiveness]

--- src/components/molecules/ChartSection.tsx
+++ src/components/molecules/ChartSection.tsx
@@ -15,7 +15,7 @@ const progress = 50; // green
 const ChartSection = () => {
   return (
     <>
-      <div className="grid grid-cols-12 gap-4 w-[98%] mt-10 ">
+      <div className="grid grid-cols-12 gap-4 w-[98%] xxsm:w-full mt-10 ">
         <div className="w-full xxsm:col-span-12 md:col-span-12 lg:col-span-12 xl:col-span-8 flex flex-wrap flex-row justify-between bg-white p-5 rounded-lg">
           <ChartSectionHearder />
           <ChartCardList />
